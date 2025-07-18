00e36cf3f3acb2b747ac22d5fe15a31c1f22f203
mostafa-al-hassan <mostafadaud014014@gmail.com>
2025-07-18 17:54:05 +0300

    commiting changes

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,11 +1,20 @@
-import React from "react";
-
-
-function Main(){
-    return(
-    <main>
-
-    </main>);
+import styles from './css/Main.module.css'
+import food from '../Assets/restauranfood.jpg'
+function Main() {
+    return (
+        <div className={styles.background}>
+            <main className={styles.main}>
+                <div className={styles.div1}>
+                    <h1>Little Lemon</h1>
+                    <h2>Chicago</h2>
+                    <p>We are a family owned Mediterranean restaurnat, focused on traditional recipes served with a modern twist.</p>
+                </div>
+                <div className={styles.div2}>
+                    <img src={food} alt='food' className={styles.img} />
+                </div>
+            </main>
+        </div>
+    );
 }
 
 export default Main;
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,16 +1,17 @@
 import logo from '../Assets/Logo.svg'
+import styles from './css/Nav.module.css'
 
 function Nav() {
     return (
-        <nav>
-            <a href="/"><img src={logo} alt="logo" /></a>
+        <nav className={styles.nav}>
+            <a href="/"><img src={logo} alt="logo" className={styles.img}/></a>
             <ul>
-                <li><a href=''>Home</a></li>
-                <li><a href=''>About</a></li>
-                <li><a href=''>Menu</a></li>
-                <li><a href=''>Reservations</a></li>
-                <li><a href=''>Order Online</a></li>
-                <li><a href=''>Login</a></li>
+                <li><a href='/'>Home</a></li>
+                <li><a href='/'>About</a></li>
+                <li><a href='/'>Menu</a></li>
+                <li><a href='/'>Reservations</a></li>
+                <li><a href='/'>Order Online</a></li>
+                <li><a href='/'>Login</a></li>
             </ul>
         </nav>
     );
